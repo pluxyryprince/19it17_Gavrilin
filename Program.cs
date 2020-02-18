c45183c454bb593f44941f6abbe7dcf1ceb707b8
pluxurypr9nce <60029576+pluxyryprince@users.noreply.github.com>
2020-02-18 09:38:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,72 @@
 ﻿using System;
 
-namespace ConsoleApp10
+namespace ConsoleApp12
 {
     class Program
-    {   //Задание: R максимально, R кратно 14, R произведение 2х различных элементов последовательности. Если такого числа нет вывести -1.
+    {
 
         static void Main(string[] args)
         {
-            Random rand = new Random(); //переменная для случайных чисел
-            int[] mas = new int[1000]; //массив на 1000 элементов
-            for (int i = 0; i < 1000; i++)
+
             {
-                mas[i] = rand.Next(0, 10000);
-                Console.WriteLine(mas[i]);
+                Console.WriteLine("Ввведите кол-во элементов в массиве:");
+                //задаем массив
+
+                int n = int.Parse(Console.ReadLine());
+                int[] a = new int[n];
+
+                //переменная для рандомных чисел 
+                var rand = new Random();
+
+                //рандом чисел от 0 до 1000
+                for (var i = 0; i < n; i++)
+                    a[i] = rand.Next(0, 1000);
+
+                //выводим первоначальный массив
+                Console.WriteLine("Превоначальный массив:");
+                for (var i = 0; i < a.Length; i++)
+                {
+                    Console.Write($"[{i}]=");
+                    Console.WriteLine(a[i]);
+                }
+                //делаем сортировку по возрастанию
+                Console.WriteLine("Сортировка по возрастанию: ");
+                Array.Sort(a);
+                for (var i = 0; i < a.Length; i++)
+                {
+                    Console.Write($"[{i}]=");
+                    Console.WriteLine("{0} ", a[i]);
+                }
+
+                //сортировка для вывода 1,2,3 места
+                int v = a[0];
+                int b = a[1];
+                int c = a[2];
+                for (int i = 3; i < n; i++)
+                {
+                    if (a[i] >= v || a[i] >= b || a[i] >= c)
+                    {
+                        if (v <= b && v <= c)
+                        {
+                            v = a[i];
+                        }
+                        else
+                        {
+                            if (b <= c)
+                            {
+                                b = a[i];
+                            }
+                            else
+                            {
+                                c = a[i];
+                            }
+                        }
+                    }
+                }
+                //выводим в консоли 3 первых места
+                Console.Write($"3 первых места: 1-{v} 2-{c} 3-{b} ");
+                Console.ReadLine();
             }
-            int R = mas[0];
-
-               Console.WriteLine("Максимальное число: ");
-
-                 for (int i = 0; i < mas.Length; i++)
-                        { if (mas[i] > R)
-                              { R = mas[i]; }   }
-                
-          
-              Console.WriteLine(R);
-
-                 Console.WriteLine("Вывод чисел после проверки условия: ");
-
-                int a = mas[new Random().Next(0, mas.Length)];
-                int b = mas[new Random().Next(0, mas.Length)];
-
-            int c = a * b;
-            if ((R % 14 == 0) && (R == c && (a - b <= 8 || b - a <= 8)))
-
-                  { Console.WriteLine(R);  }
-         
-
-                    else
-                          { Console.WriteLine("-1");  }
-                           
         }
     }
 }
